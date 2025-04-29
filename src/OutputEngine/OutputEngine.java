a30876827f94b108fe3d80f22610fc4e50514bae
3akare <bakaredavid007@gmail.com>
2025-04-29 14:03:05 +0100

    feat [output engine]: added output engine

---
+++ src/OutputEngine/OutputEngine.java
@@ -0,0 +1,13 @@
+package OutputEngine;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class OutputEngine {
+    public OutputEngine(){}
+    public void outputMessage(String outputMessage, double rollingAverage){
+        System.out.println("[Output Engine]: Average rolling " +
+                (outputMessage != null ? outputMessage: "") +
+                BigDecimal.valueOf(rollingAverage).setScale(2, RoundingMode.HALF_UP).doubleValue());
+    }
+}
